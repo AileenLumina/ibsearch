1d82d08f081a4220bff84ab0cabb4684b4da9895
martmists <martmists@gmail.com>
2016-11-22 18:55:36 +0100

    Code improvements
    - Add test for nsfw requests
    - `search` can be used both async and sync using kwarg `async_`
    - `async_search` makes use of the sync/async ability of `search`
    - Parameters are built in `_build_params`
    - `search` will return a generator in a sync context, call `next()` on
    it to get the result.
    - Add .gitignore

--- ibsearch.py
+++ ibsearch.py
@@ -24,7 +24,7 @@ class Image:
 class IbSearch:
     def __init__(self, api_key, loop=None):
         self.api_key = api_key
-        self.headers = {'X-IbSearch-Key': self.api_key}
+        self.headers = {'X-IbSearch-Key': api_key}
         self.session = None
         if loop:
             self.loop = loop
@@ -32,19 +32,24 @@ class IbSearch:
     @asyncio.coroutine
     def _async_request(self, url, params=None):
         params = params or {}
+        
         try:
             import aiohttp
         except ImportError:
             raise Exception("Aiohttp has to be installed to use this function.")
         else:
+
             with aiohttp.ClientSession(loop=self.loop) as session:
                 res = yield from session.get(url, params=params, headers=self.headers)
+
                 if not res.status == 200:
                     raise UnexpectedResponseCode(res.status, (yield from res.text()))
+
                 try:
                     result = yield from res.json()
                 finally:
                     yield from res.release()
+
             return result
 
     def _request(self, url, params=None):
@@ -60,15 +65,8 @@ class IbSearch:
             result = res.json()
             return result
 
-    @asyncio.coroutine
-    def async_search(self, query, *, limit=None, page=1, 
-                           nsfw_allowed=False, shuffle=False, 
-                           shuffle_limit=None):
-        if nsfw_allowed:
-            domain = DOMAIN_NSFW
-        else:
-            domain = DOMAIN_REGULAR
-        # building querystring
+    @staticmethod
+    def _build_params(query, limit, page, shuffle, shuffle_limit):
         params = {
             'q': query
         }
@@ -82,50 +80,42 @@ class IbSearch:
                 params['shuffle'] = shuffle_limit
             # Because this does not appear to work on IbSearch's end at the time of writing,
             # it needs to be done locally (see below).
-        result = yield from self._async_request("http://" + domain + IMAGES_PATH, params)
-        try:
-            result[0]
-        except IndexError:
-            raise NoResults
-        images = [Image(domain, **d) for d in result]
-        if shuffle:
-            images = self.shuffle(images, shuffle_limit)
-        return images
-    
+        return params
+
+	@asyncio.coroutine
+	def async_search(self, *args):
+		yield from self.search(*args, async_=True)
+
     def search(self, query, *, limit=None, page=1, nsfw_allowed=False,
                shuffle=False, shuffle_limit=None, async_=False):
-        if async_:
-            return self.async_search(query, limit=limit, page=page,
-                                     nsfw_allowed=nsfw_allowed,
-                                     shuffle=shuffle, 
-                                     shuffle_limit=shuffle_limit)
         if nsfw_allowed:
             domain = DOMAIN_NSFW
         else:
             domain = DOMAIN_REGULAR
-        # building querystring
-        params = {
-            'q': query,
-        }
-        if limit:
-            params['limit'] = limit
-        if page is not 1:
-            params['page'] = page
-        if shuffle:
-            params['shuffle'] = True
-            if shuffle_limit:
-                params['shuffle'] = shuffle_limit
-            # Because this does not appear to work on IbSearch's end at the time of writing,
-            # it needs to be done locally (see below).
-        result = self._request("http://" + domain + IMAGES_PATH, params)
+
+        params = self._build_params(query, limit, page, shuffle, shuffle_limit)
+
+        url = "http://" + domain + IMAGES_PATH
+
+        if async_:
+            result = yield from self._async_request(url, params)
+        else:
+            result = self._request(url, params)
+
         try:
             result[0]
         except IndexError:
             raise NoResults
+
         images = [Image(domain, **d) for d in result]
+
         if shuffle:
             images = self.shuffle(images, shuffle_limit)
-        return images
+
+        if async_:
+            return images
+        else:
+            yield images
 
     def shuffle(self, images, limit):
         random.shuffle(images)
@@ -134,14 +124,16 @@ class IbSearch:
                 images = images[0:limit]
             except IndexError:
                 pass # Just return the shuffled list
+
         return images
 
     def get_random_image(self, query, nsfw_allowed=False, async_=False):
         if async_:
             return self.async_get_random_image(query, nsfw_allowed=nsfw_allowed)
-        image_list = self.search(query, limit=100, nsfw_allowed=nsfw_allowed, shuffle=True, shuffle_limit=1, async_=async_)
+        image_list = self.search(query, limit=100, nsfw_allowed=nsfw_allowed, shuffle=True, 
+                                 shuffle_limit=1, async_=False)
         try:
-            image = image_list[0]
+            image = next(image_list)[0]
             return image
         except IndexError:
             # Not supposed to happen but here just in case
@@ -149,7 +141,8 @@ class IbSearch:
 
     @asyncio.coroutine
     def async_get_random_image(self, query, nsfw_allowed=False):
-        image_list = yield from self.async_search(query, limit=100, nsfw_allowed=nsfw_allowed, shuffle=True, shuffle_limit=1)
+        image_list = yield from self.search(query, limit=100, nsfw_allowed=nsfw_allowed, 
+                                                  shuffle=True, shuffle_limit=1, async_=True)
         try:
             image = image_list[0]
             return image
--- test.py
+++ test.py
@@ -13,10 +13,13 @@ def test():
 @asyncio.coroutine
 def async_test():
     image2 = yield from cl.get_random_image(test_query, async_=True)
-    image3 = yield from cl.async_get_random_image(test_query)
     print(image2.url)
+    
+def nsfw_test():
+    image3 = cl.get_random_image(test_query, nsfw_allowed=True)
     print(image3.url)
 
 if __name__ == '__main__':
     test()
     loop.run_until_complete(async_test())
+	nsfw_test()
